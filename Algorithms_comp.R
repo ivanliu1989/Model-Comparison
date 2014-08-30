11f2aa4f6da86d30cd74a590946ba78e6f1c43d1
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-08-30 22:55:50 +1000

    lambda

--- Algorithms_comp.R
+++ Algorithms_comp.R
@@ -8,3 +8,22 @@ train.y <- dtm[training.indices, 1]
 test.x <- dtm[test.indices, 3:ncol(dtm)]
 test.y <- dtm[test.indices, 1]
 rm(dtm)
+
+library(glmnet)
+regularized.logit.fit <- glmnet(train.x, train.y, family = c('binomial'))
+lambdas <- regularized.logit.fit$lambda
+performance <- data.frame()
+for (lambda in lambdas)
+{
+    predictions <- predict(regularized.logit.fit, test.x, s = lambda)
+    predictions <- as.numeric(predictions > 0)
+    mse <- mean(predictions != test.y)
+    performance <- rbind(performance, data.frame(Lambda = lambda, MSE = mse))
+}
+png('Lambda.png')
+ggplot(performance, aes(x = Lambda, y = MSE)) +
+    geom_point() +
+    scale_x_log10()
+dev.off()
+# best point
+best.lambda <- with(performance, max(Lambda[which(MSE == min(MSE))]))
