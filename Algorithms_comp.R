84c16c196a28f0058ed60bcf1497656f27ff1488
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-08-30 22:48:12 +1000

    algorithms-comparison

--- Algorithms_comp.R
+++ Algorithms_comp.R
@@ -1,2 +1,10 @@
 setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Model-Comparison')
-
+load('data/dtm.RData')
+set.seed(1)
+training.indices <- sort(sample(1:nrow(dtm), round(0.5 * nrow(dtm))))
+test.indices <- which(! 1:nrow(dtm) %in% training.indices)
+train.x <- dtm[training.indices, 3:ncol(dtm)]
+train.y <- dtm[training.indices, 1]
+test.x <- dtm[test.indices, 3:ncol(dtm)]
+test.y <- dtm[test.indices, 1]
+rm(dtm)
